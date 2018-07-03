33a6807e945919b3c4e04f79c6334270ac69a2cb
brianchase <brianchase@mail.com>
2018-07-03 14:22:13 -0500

    major overhaul; includes new menu for listing clients

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ This Bash script helps with managing
 [VPN](https://en.wikipedia.org/wiki/Virtual_private_network)
 connections with [OpenVPN](https://openvpn.net/).
 
+## PORTABILITY
+
+Since the script uses arrays, it's not strictly
+[POSIX](https://en.wikipedia.org/wiki/POSIX)-compliant. As a result,
+it isn't compatible with
+[Dash](http://gondor.apana.org.au/~herbert/dash/) and probably a good
+number of other shells.
+
 ## LICENSE
 
 This project is in the public domain under [The
--- vpn-client.sh
+++ vpn-client.sh
@@ -9,29 +9,21 @@ PC[1]="b.dummy.client"
 PC[2]="c.dummy.client"
 PC[3]="d.dummy.client"
 
-# Useful for scripts (see my online-netctl.sh):
-vpn_stopnow () {
-  printf '%s\n' "Stopping OpenVPN client $DC..."
-  vpn_arg stop
-}
-
 vpn_stop () {
-  read -r -p "Stop OpenVPN client $DC? [y/n] " DN
-  if [ "$DN" = y ]; then
-    vpn_stopnow
+  if [ "$1" != now ]; then
+    read -r -p "Stop OpenVPN client $CL? [y/n] " DN
   fi
-}
-
-vpn_arg () {
-  if systemctl "$1" openvpn-client@"$DC"; then
-    chk_status "$1"
+  if [ "$DN" = y ] || [ "$1" = now ]; then
+    vpn_arg stop
   fi
 }
 
 vpn_restart () {
   if chk_online; then
-    read -r -p "Restart OpenVPN client $DC? [y/n] " RV
-    if [ "$RV" = y ]; then
+    if [ "$1" != now ]; then
+      read -r -p "Restart OpenVPN client $CL? [y/n] " RV
+    fi
+    if [ "$RV" = y ] || [ "$1" = now ]; then
       vpn_arg restart
     fi
   fi
@@ -40,13 +32,13 @@ vpn_restart () {
 vpn_op () {
   until [ "$OP" ]; do
     printf '%s\n\n' "Please choose:"
-    printf '\t%s\n' "1. Stop OpenVPN client $DC"
-    printf '\t%s\n' "2. Restart OpenVPN client $DC"
-    printf '\t%s\n' "3. Exit"
+    printf '\t%s\n' "1. Stop OpenVPN client $CL"
+    printf '\t%s\n' "2. Restart OpenVPN client $CL"
+    printf '\t%s\n' "3. Skip"
     read -r OP
     case $OP in
-      1) vpn_stop ;;
-      2) vpn_restart ;;
+      1) vpn_stop now ;;
+      2) vpn_restart now ;;
       3) break ;;
       *) unset OP ;;
     esac
@@ -60,74 +52,99 @@ chk_online () {
   fi
 }
 
-vpn_client () {
-  until [ "$DC" ]; do
-    if [ "${#PC[*]}" -eq 0 ]; then
+client_loop () {
+  until [ "$CL" ]; do
+    if [ -z "$DC" ] && [ "${#PC[*]}" -eq 0 ]; then
       printf '%s\n' "Could not start OpenVPN! No listed clients!"
       return 1
-    elif [ "${#PC[*]}" -eq 1 ]; then
-      DC="${PC[*]}"
+    elif [ -z "$DC" ] && [ "${#PC[*]}" -eq 1 ]; then
+      CL="${PC[0]}"
+    elif [ "$DC" ] && [ "${#PC[*]}" -eq 0 ]; then
+      CL="$DC"
+    elif [ "${#PC[*]}" -eq 1 ] && [ "$DC" = "${PC[*]}" ]; then
+      CL="$DC"
     else
       N=0
       printf '%s\n\n' "Please choose:"
-      for i in "${!PC[@]}"; do
-        printf '\t%s\n' "$((N += 1)). ${PC[i]}"
-      done
-      printf '\t%s\n' "$((N += 1)). Exit"
-      read -r OP
-      case $OP in
-        ''|*[!1-9]*) continue ;;
-      esac
-      if [ "$OP" -gt "$N" ]; then
-        continue
-      elif [ "$OP" -eq "$N" ]; then
-        return 1
+      if [ "$DC" ]; then
+        for i in "${PC[@]}"; do
+          if [ "$i" = "$DC" ]; then
+            for j in "${!PC[@]}"; do
+              if [ "${PC[$j]}" = "$i" ]; then
+                unset "PC[$j]"
+                PC=("${PC[@]}")
+                break;
+              fi
+            done
+          fi
+        done
+        PC=("$DC" "${PC[@]}")
+        for j in "${!PC[@]}"; do
+          if [ "$j" = 0 ]; then
+            printf '\t%s\n' "$((N += 1)). OpenVPN client ${PC[j]} [default]"
+          else
+            printf '\t%s\n' "$((N += 1)). OpenVPN client ${PC[j]}"
+          fi
+        done
+        printf '\t%s\n' "$((N += 1)). Skip"
+        read -r OP
+        case $OP in
+          *[!1-9]*) continue ;;
+        esac
+        if [ -z "$OP" ]; then
+          CL="${PC[0]}"
+        elif [ "$OP" -gt "$N" ]; then
+          continue
+        elif [ "$OP" -ne "$N" ]; then
+          CL="${PC[(($OP - 1))]}"
+        fi
       else
-        DC="${PC[(($OP - 1))]}"
+        for i in "${!PC[@]}"; do
+          printf '\t%s\n' "$((N += 1)). OpenVPN client ${PC[i]}"
+        done
+        printf '\t%s\n' "$((N += 1)). Skip"
+        read -r OP
+        case $OP in
+          ''|*[!1-9]*) continue ;;
+        esac
+        if [ "$OP" -gt "$N" ]; then
+          continue
+        elif [ "$OP" -ne "$N" ]; then
+          CL="${PC[(($OP - 1))]}"
+        fi
+      fi
+      if [ "$OP" ] && [ "$OP" -eq "$N" ]; then
+        return 1
       fi
     fi
   done
 }
 
-vpn_active_client () {
-  DC="$(systemctl status openvpn-client@* | grep -oP 'OpenVPN tunnel for \K.*')"
-}
-
-chk_status () {
-  if systemctl is-active -q openvpn-client@"$DC"; then
-    printf '%s\n' "OpenVPN client $DC is active"
-  else
-    printf '%s\n' "OpenVPN client $DC is inactive"
+vpn_arg () {
+  if ! systemctl "$1" openvpn-client@"$CL"; then
+    printf '%s\n' "Failed to $1 OpenVPN client $CL!"
+    return 1
   fi
 }
 
 vpn_start () {
-  if chk_online; then
-    if vpn_client; then
-      read -r -p "Start OpenVPN client $DC? [y/n] " UP
-      if [ "$UP" = y ]; then
-        vpn_arg start
-      fi
-    fi
+  if chk_online && client_loop; then
+    vpn_arg start
   fi
 }
 
 vpn_main () {
   if systemctl is-active -q openvpn-client@*; then
-    vpn_active_client
+    CL="$(systemctl status openvpn-client@* | grep -oP 'OpenVPN tunnel for \K.*')"
     case $1 in
-      restart) vpn_restart ;;
-      status) printf '%s\n' "OpenVPN active client: $DC" ;;
-      stop) vpn_stop ;;
-      stopnow) vpn_stopnow ;;
+      restart) vpn_restart "$2" ;;
+      status) printf '%s\n' "OpenVPN client $CL is active" ;;
+      stop) vpn_stop "$2" ;;
       *) vpn_op ;;
     esac
   else
     case $1 in
-      restart) printf '%s\n' "OpenVPN is inactive"
-               chk_online ;;
-      status|stop) printf '%s\n' "OpenVPN is inactive" ;;
-      stopnow) ;;
+      restart|status|stop) printf '%s\n' "No OpenVPN client is active" ;;
       *) vpn_start ;;
     esac
   fi
