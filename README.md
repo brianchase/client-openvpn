103b44eacd9d276f6529aec34a50488764fbd72d
brianchase <brianmichaelchase@gmail.com>
2020-03-15 13:37:36 -0500

    simplify

--- README.md
+++ README.md
@@ -8,43 +8,38 @@ connections with [OpenVPN](https://openvpn.net "OpenVPN").
 
 ## How It Works
 
-On line six of the script, you may specify a default VPN client by
-making it the value of the variable `DClient`:
+You may specify a default VPN client by making it the value of the
+variable `DClient`:
 
 ```
 #DClient="a.dummy.client"
 ```
 
-You may specify other clients by adding them to the array `PClients`
-(possible clients) that starts on line nine:
+You may specify other clients by adding them to the array `PClient`
+(the *p* is for *possible*):
 
 ```
-PClients[0]="a.dummy.client"
-PClients[1]="b.dummy.client"
-PClients[2]="c.dummy.client"
-PClients[3]="d.dummy.client"
+PClient[0]="a.dummy.client"
+PClient[1]="b.dummy.client"
+PClient[2]="c.dummy.client"
+PClient[3]="d.dummy.client"
 ```
 
-If you want the script to start clients, you need to list at least one
-valid client at the top of the script, either as a default or as a
-"possible client." This is because users don't typically have access
-to `/etc/openvpn/client/`, where client configuration files reside, so
+To start clients, you need to list at least one valid `DClient` or
+`PClient`. This is because users don't typically have access to
+`/etc/openvpn/client/`, where client configuration files reside, so
 the script doesn't attempt to determine which clients are available to
 start. You need to tell it, manually.
 
-One more condition for starting a client is an internet connection. If
-you don't have an internet connection, you can only use the script to
-stop clients or check their status.
-
-Suppose, then, that you listed at least one valid client at the top of
-the script and have an internet connection. If you run the script
-without options, and no client is active, the script asks to start a
-client. If you listed only one client at the top of the script, the
-script asks to start it. If you listed more than one, the script
-prints that list. You may choose one to start or choose "skip" to exit
-the script. A default client, if you gave one, appears at the top of
-the list with the flag `[default]`. You can simply press Enter to
-start it.
+Suppose, then, that you've listed at least one valid client. If you
+run the script without options, and no client is active, the script
+asks to start a client. If you've listed only one client (whether as a
+`DClient` or `PClient` or both makes no difference), the script asks
+to start that client. If you've listed more than one, the script
+prints a numbered list of them, and you may choose one to start or
+choose "skip" to exit the script. A default client, if you gave one,
+appears at the top of the list with the flag `[default]`. You may
+simply press Enter to start it.
 
 If you run the script without options, and a client is active, the
 script asks if you want to stop or restart the active client or "skip"
@@ -57,8 +52,7 @@ $ client-openvpn.sh [restart|start|stop] [now]
 ```
 
 Use `restart`, `start`, or `stop` to perform those actions on a
-client, bypassing menus, if applicable. Adding `now` bypasses prompts
-for confirmation.
+client. Adding `now` bypasses prompts for confirmation.
 
 If no client is active, `restart` and `restart now` give error
 messages, then ask to start a profile, if you've listed any. By
--- client-openvpn.sh
+++ client-openvpn.sh
@@ -11,44 +11,27 @@ PClients[1]="b.dummy.client"
 PClients[2]="c.dummy.client"
 PClients[3]="d.dummy.client"
 
-vpn_op () {
-  until [ "$Opt" ]; do
-    printf '%s\n\n\t%s\n\t%s\n\t%s\n' "Please choose:" \
-      "1. Stop OpenVPN client $Client" \
-      "2. Restart OpenVPN client $Client" \
-      "3. Skip"
-    local Opt
-    read -r Opt
-    case $Opt in
-      1) vpn_arg stop ;;
-      2) vpn_arg restart ;;
-      3) return 1 ;;
-      *) unset Opt ;;
-    esac
-  done
-}
-
 client_loop () {
   until [ "$Client" ]; do
     local DefTag N=0 Opt i j
     printf '%s\n\n' "Please choose:"
     if [ "$DClient" ]; then
 # If DClient has a value, make it Profiles[0].
-      for i in "${!PClients[@]}"; do
-        if [ "${PClients[$i]}" = "$DClient" ]; then
-          unset "PClients[$i]"
-          PClients=("${PClients[@]}")
+      for i in "${!PClient[@]}"; do
+        if [ "${PClient[$i]}" = "$DClient" ]; then
+          unset "PClient[$i]"
+          PClient=("${PClient[@]}")
           break
         fi
       done
-      PClients=("$DClient" "${PClients[@]}")
+      PClient=("$DClient" "${PClient[@]}")
       DefTag=" [default]"
     fi
 # Now build the rest of the menu.
-    printf '\t%s\n' "$((N += 1)). Start OpenVPN client ${PClients[0]}$DefTag"
-    for j in "${!PClients[@]}"; do
+    printf '\t%s\n' "$((N += 1)). Start OpenVPN client ${PClient[0]}$DefTag"
+    for j in "${!PClient[@]}"; do
       if [ "$j" != 0 ]; then
-        printf '\t%s\n' "$((N += 1)). Start OpenVPN client ${PClients[j]}"
+        printf '\t%s\n' "$((N += 1)). Start OpenVPN client ${PClient[j]}"
       fi
     done
     printf '\t%s\n' "$((N += 1)). Skip"
@@ -60,89 +43,76 @@ client_loop () {
     if [ -z "$Opt" ] && [ -z "$DClient" ]; then
       continue
     elif [ -z "$Opt" ]; then
-      Client="${PClients[0]}"
+      Client="${PClient[0]}"
     elif [ "$Opt" -ge 1 ] && [ "$Opt" -lt "$N" ]; then
-      Client="${PClients[(($Opt - 1))]}"
+      Client="${PClient[(($Opt - 1))]}"
     fi
   done
 }
 
-vpn_start () {
-  if [ -z "$DClient" ] && [ "${#PClients[*]}" -eq 0 ]; then
+vpn_client () {
+  if [ -z "$DClient" ] && [ "${#PClient[*]}" -eq 0 ]; then
     printf '%s\n' "No listed OpenVPN clients!" >&2
     return 1
-  elif [ -z "$DClient" ] && [ "${#PClients[*]}" -eq 1 ]; then
-    Client="${PClients[*]}"
-  elif [ "$DClient" ] && [ "${#PClients[*]}" -eq 0 ]; then
+  elif [ -z "$DClient" ] && [ "${#PClient[*]}" -eq 1 ]; then
+    Client="${PClient[*]}"
+  elif [ "$DClient" ] && [ "${#PClient[*]}" -eq 0 ]; then
     Client="$DClient"
-  elif [ "${#PClients[*]}" -eq 1 ] && [ "$DClient" = "${PClients[*]}" ]; then
+  elif [ "${#PClient[*]}" -eq 1 ] && [ "$DClient" = "${PClient[*]}" ]; then
     Client="$DClient"
   else
-# If there's more than one client, choose which one to start.
-    client_loop && vpn_arg start
-    return
+    set -- "start" "now"
+    client_loop || return 1
   fi
-# If there's just one client, set about starting it.
-  vpn_confirm start "$1"
+  vpn_arg start "$2"
+}
+
+vpn_op () {
+  until [ "$Opt" ]; do
+    printf '%s\n\n\t%s\n\t%s\n\t%s\n' "Please choose:" \
+      "1. Stop OpenVPN client $Client" \
+      "2. Restart OpenVPN client $Client" \
+      "3. Skip"
+    local Opt
+    read -r Opt
+    case $Opt in
+      1) vpn_arg stop now ;;
+      2) vpn_arg restart now ;;
+      3) return 1 ;;
+      *) unset Opt ;;
+    esac
+  done
 }
 
 vpn_arg () {
+  if [ "$2" != now ]; then
+    local Confirm
+    read -r -p "${1^} OpenVPN client $Client? [y/n] " Confirm
+    [ "$Confirm" = y ] || return 1
+  fi
   if ! systemctl "$1" openvpn-client@"$Client"; then
     printf '%s\n' "Failed to $1 OpenVPN client $Client!" >&2
     return 1
   fi
 }
 
-vpn_confirm () {
-  if [ "$2" != now ]; then
-    local Confirm
-    read -r -p "${1^} OpenVPN client $Client? [y/n] " Confirm
-    [ "$Confirm" = y ] || return 1
-  fi
-  vpn_arg "$1"
-}
-
-vpn_online () {
-  if wget -q --tries=10 --timeout=8 --spider http://google.com; then
-    if [ "$1" = restart ]; then
-# An internet connection, OpenVPN is active, and '$1' is 'restart'.
-      vpn_confirm restart "$2"
-    elif [ "$Client" ]; then
-# An internet connection and OpenVPN is active.
-      vpn_op
-    else
-# An internet connection and OpenVPN is inactive.
-      vpn_start "$2"
-    fi
-  elif [ "$Client" ]; then
-# No internet connection and OpenVPN is active.
-    printf '%s\n' "OpenVPN client $Client is active but offline!" >&2
-    vpn_confirm stop
-  else
-# No internet connection and OpenVPN is inactive.
-    printf '%s\n' "No internet connection or active OpenVPN client!" >&2
-    return 1
-  fi
-}
-
 vpn_main () {
-  if systemctl is-active -q openvpn-client@*; then
-    Client="$(systemctl list-units -t service --state=running | grep -oP 'OpenVPN tunnel for \K.*\b')"
+  Client="$(systemctl list-units -t service --state=running | grep -oP 'OpenVPN tunnel for \K.*\b')"
+  if [ "$Client" ]; then
     case $1 in
-      stop) vpn_confirm "$1" "$2" ;;
-      *) vpn_online "$1" "$2" ;;
+      restart|stop) vpn_arg "$1" "$2" ;;
+      *) vpn_op ;;
     esac
   else
     case $1 in
-      restart) printf "No active OpenVPN client! " >&2
-               vpn_start "$2" ;;
+      restart) printf "No active OpenVPN client! " >&2 ;;
       stop) case $2 in
               now) return 1 ;;
               *) printf '%s\n' "No active OpenVPN client!" >&2
                  return 1 ;;
             esac ;;
-      *) vpn_online "$1" "$2" ;;
     esac
+    vpn_client start "$2"
   fi
 }
 
